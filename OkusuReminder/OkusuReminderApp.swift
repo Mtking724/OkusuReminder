e04a72b04364df1da431aec2c2f1232841c6a05d
motoki0724 <motokiokayasu@gmail.com>
2023-10-24 06:51:52 +0900

    Initial Commit

---
+++ OkusuReminder/OkusuReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  OkusuReminderApp.swift
+//  OkusuReminder
+//
+//  Created by Motoki Okayasu on 2023/10/24.
+//
+
+import SwiftUI
+
+@main
+struct OkusuReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
