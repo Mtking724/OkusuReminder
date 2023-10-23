ee65950ee32cddf006ecd20523afda9247e25b73
motoki0724 <motokiokayasu@gmail.com>
2023-10-24 06:55:41 +0900

    MVVMデータモデルを採用。各グループとファイルを作成。

---
+++ OkusuReminder/View/FirstView.swift
@@ -0,0 +1,20 @@
+//
+//  FirstView.swift
+//  OkusuReminder
+//
+//  Created by Motoki Okayasu on 2023/10/24.
+//
+
+import SwiftUI
+
+struct FirstView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FirstView_Previews: PreviewProvider {
+    static var previews: some View {
+        FirstView()
+    }
+}
---
+++ OkusuReminder/ViewModel/FirstViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FirstViewModel.swift
+//  OkusuReminder
+//
+//  Created by Motoki Okayasu on 2023/10/24.
+//
+
+import Foundation
