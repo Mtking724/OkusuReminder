16fb3ebc32a55c09b852e1739dcb89794c693f0a
motoki0724 <motokiokayasu@gmail.com>
2023-10-24 22:12:19 +0900

    FirstTAbViewに現在日時をTextで配置

--- OkusuReminder/View/FirstTabView.swift
+++ OkusuReminder/View/FirstTabView.swift
@@ -8,10 +8,33 @@
 import SwiftUI
 
 struct FirstTabView: View {
+    let currentDate = Date()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack() {
+            Text(FormattTime(currentDate))
+                .font(.largeTitle)
+                .padding()
+            Text(FormattDate(currentDate))
+                .font(.headline)
+            Spacer()
+        }
     }
 }
+
+private func FormattDate(_ date: Date) -> String {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd"
+    return dateFormatter.string(from: date)
+}
+
+private func FormattTime(_ date: Date) -> String {
+    let timeFormatter = DateFormatter()
+    timeFormatter.dateFormat = "HH:mm:ss"
+    return timeFormatter.string(from: date)
+}
+
+
 
 struct FirstTabView_Previews: PreviewProvider {
     static var previews: some View {
--- OkusuReminder/View/FirstView.swift
+++ OkusuReminder/View/FirstView.swift
@@ -11,7 +11,6 @@ struct FirstView: View {
     var body: some View {
         
         VStack {
-           
             Text("おくすリマインダー")
                 .font(.largeTitle.bold())
                 .offset(y: -40)
