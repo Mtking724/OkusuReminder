a4c1a9f16711ec712a0a712d07c958f040efe19c
motoki0724 <motokiokayasu@gmail.com>
2023-10-28 23:07:01 +0900

    ButtonStyleを実装

--- OkusuReminder/View/FirstView.swift
+++ OkusuReminder/View/FirstView.swift
@@ -11,19 +11,34 @@ struct FirstView: View {
     var body: some View {
         
         VStack {
+            //Text,Image,STARTButtonを縦にならべる
             Text("おくすリマインダー")
                 .font(.largeTitle.bold())
                 .offset(y: -40)
             Image("grandma")
                 .resizable()
                 .frame(width: 200, height: 280)
-            Capsule()
-                .frame(width: 300, height: 100)
-                .offset(y: 50)
+            Button(action: {}) {
+                Text("スタート")
+                    .font(.title2.bold())
+            }
+            .buttonStyle(RoundedButtonStyle())
+            .offset(y: 30)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color.orange)
-       
+    }
+}
+
+//ButtonStyleメソッドでボタンを汎用化
+struct RoundedButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.mint)
+            .cornerRadius(15)
+            .opacity(configuration.isPressed ? 0.2 : 1)
     }
 }
 
