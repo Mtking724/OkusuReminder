9ee0ae6ee987840e3d5e577915bf8a2e2af63b92
motoki0724 <motokiokayasu@gmail.com>
2023-10-29 00:56:50 +0900

    表紙のイメージを設定した。

--- OkusuReminder/View/FirstView.swift
+++ OkusuReminder/View/FirstView.swift
@@ -13,14 +13,22 @@ struct FirstView: View {
         VStack {
             //Text,Image,STARTButtonを縦にならべる
             Text("おくすリマインダー")
-                .font(.largeTitle.bold())
+                .font(Font.custom("MochiyPopOne-Regular", size: 35))
+                .foregroundColor(.black)
+                .font(.largeTitle)
+                .bold()
                 .offset(y: -40)
+               
+                
+                
             Image("grandma")
                 .resizable()
                 .frame(width: 200, height: 280)
+                .offset(y: -20)
             Button(action: {}) {
                 Text("スタート")
-                    .font(.title2.bold())
+                    .font(Font.custom("MochiyPopOne-Regular", size: 30))
+                    .frame(width: 180,height: 30)
             }
             .buttonStyle(RoundedButtonStyle())
             .offset(y: 30)
@@ -38,6 +46,7 @@ struct RoundedButtonStyle: ButtonStyle {
             .foregroundColor(.white)
             .background(Color.mint)
             .cornerRadius(15)
+            //ボタンを押すと透明になる。
             .opacity(configuration.isPressed ? 0.2 : 1)
     }
 }
