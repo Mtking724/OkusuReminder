bc5a76e64603818fe7a47938c78fd0e834b5f290
motoki0724 <motokiokayasu@gmail.com>
2023-10-29 20:51:02 +0900

    UIを大きく変更。

--- OkusuReminder/View/FirstTabView.swift
+++ OkusuReminder/View/FirstTabView.swift
@@ -13,12 +13,44 @@ struct FirstTabView: View {
     var body: some View {
         VStack() {
             Text(FormattTime(currentDate))
-                .font(.largeTitle)
-                .padding()
+                .font(.largeTitle.bold())
+                .frame(alignment: .top)
             Text(FormattDate(currentDate))
-                .font(.headline)
-            Spacer()
-                
+                .font(.title2.bold())
+                .frame(alignment: .top)
+               
+            Text("薬を飲んだらボタンを押しましょう！")
+                .padding()
+                .font(.title3.bold())
+                .frame(alignment: .leading)
+            ZStack {
+                RoundedRectangle(cornerRadius: 30)
+                    .frame(width: 350, height: 120)
+                    .padding()
+                Circle()
+                    .foregroundColor(.white)
+                    .frame(width: 90,height: 90, alignment: .leading)
+                    .offset(x: -120)
+            }
+            ZStack {
+                RoundedRectangle(cornerRadius: 30)
+                    .frame(width: 350, height: 120)
+                    .padding()
+                Circle()
+                    .foregroundColor(.white)
+                    .frame(width: 90,height: 90, alignment: .leading)
+                    .offset(x: -120)
+            }
+            ZStack {
+                RoundedRectangle(cornerRadius: 30)
+                    .frame(width: 350, height: 120)
+                    .padding()
+                Circle()
+                    .foregroundColor(.white)
+                    .frame(width: 90,height: 90, alignment: .leading)
+                    .offset(x: -120)
+            }
+            
             }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color.orange)
